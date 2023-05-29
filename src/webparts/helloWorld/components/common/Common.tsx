27383259822394684d6c73d465ca0deeebd6cc34
Janne Holm <janne.holm@meltlake.com>
2023-05-29 12:55:03 +0300

    Just changing the import

--- src/webparts/helloWorld/components/common/Common.tsx
+++ src/webparts/helloWorld/components/common/Common.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { DefaultButton } from 'office-ui-fabric-react';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 
 const TestButton = (): JSX.Element => {
   return <DefaultButton>Test</DefaultButton>;
